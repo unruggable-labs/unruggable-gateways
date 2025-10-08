0fb5e2797992dd9259801bb3e976fe96b35e994d
Thomas Clowes <thomas@doublenegative.com>
2025-10-08 17:46:59 +0100

    Documented an incident with Base relating to an upgrade they have made to their AnchorStateRegistry (putting it behind a proxy). Finalized Base gateway CI tests have been disabled.

--- test/gateway/unfinalized-base.test.ts
+++ test/gateway/unfinalized-base.test.ts
@@ -5,5 +5,5 @@ testOPFault(OPFaultRollup.baseMainnetConfig, {
   slotDataContract: '0x0C49361E151BC79899A9DD31B8B0CCdE4F6fd2f6',
   slotDataPointer: '0x972433d30b6b78C05ADf32972F7b8485C112E055',
   minAgeSec: 21600,
-  skipCI: true,
+  skipCI: false,
 });
